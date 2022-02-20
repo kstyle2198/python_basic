aad35cbfd19e75eb04ba66f2741799f18a5b6aec
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-02-20 10:29:38 +0900

    go or stop function

--- pyautogui1.py
+++ pyautogui1.py
@@ -3,6 +3,17 @@ import time
 from slack import *
 
 
+# 계속 진행여부 체크 함수
+def go_or_stop(n):
+    go_check = gui.confirm(
+        text="계속진행하시겠습니까?", title="진행여부 체크", buttons=("Go", "Stop"))
+    if go_check == "Stop":
+        print(f"{n}번 작업중지")
+        quit()
+    else:
+        print(f"{n}번 작업후 계속 진행")
+        pass
+
 # gui.moveTo(500, 500, 3)
 # print("작업1")
 # a = gui.locateOnScreen('./images/check_box.png')
@@ -36,19 +47,21 @@ from slack import *
 # print("작업완료")
 
 
-# 33
-
 # 좌상단 체크박스부터 순차적으로 체크하면서 작업 수행하기
 chk_boxs = list(gui.locateAllOnScreen('./images/check_box.png'))
+print(f"총 작업대상 개수는 {len(chk_boxs)}개 입니다.")
 gui.moveTo(500, 500)
 gui.click()
 print("작업개시")
+time.sleep(2)
+n = 1
 for i in chk_boxs:
     gui.moveTo(gui.locateCenterOnScreen('./images/check_box.png'))
     time.sleep(0.1)
     gui.click()
     time.sleep(0.1)
+    go_or_stop(n)
+    n += 1
 
-    print("다음 작업을 합니다.")
 print("작업종료")
 Slack_Msg("작업이 정상적으로 종료되었습니다.")
