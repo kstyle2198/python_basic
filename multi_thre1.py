23a9a75444c1912500a795f31a8034e816d5d6e4
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-04-08 06:59:18 +0900

    multi thread and processing

--- multi_thre1.py
+++ multi_thre1.py
@@ -1,14 +1,19 @@
 import requests
 import time
 import threading
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import os
 
 #  multi threading을 사용한 비동기적 처리 방식
 
+
 def fetcher(params):
-    print(params)
+    # print(params)
     session = params[0]
+    # print(session)
     url = params[1]
+    # print(url)
+    print(f"{os.getpid()} process | {threading.get_ident()} url : {url}")
     with session.get(url) as response:
         return response.text
 
@@ -16,12 +21,15 @@ def fetcher(params):
 def main():
     urls = ["https://naver.com", "https://google.com", "https://daum.net"]
 
-    executor = ThreadPoolExecutor(max_workers=1)
+    executor = ThreadPoolExecutor(max_workers=3)
+    # executor = ProcessPoolExecutor(max_workers=3)
 
     with requests.Session() as session:
         # result = [fetcher(session, url) for url in urls]
         # print(result)
-        executor.map(fetcher, urls)
+        params = [(session, url) for url in urls]
+        results = list(executor.map(fetcher, params))
+        # print(results)
 
 
 if __name__ == "__main__":
